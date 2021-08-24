1bc662134f7ceb2639e38f85921c40bdbd7a76da
Zerceus <86751682+Zerceus@users.noreply.github.com>
2021-08-24 21:14:21 +0800

    wip

--- GameAssignmentProject/Assets/Script/PlayerController.cs
+++ GameAssignmentProject/Assets/Script/PlayerController.cs
@@ -4,25 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private bool Ground;
-    bool crouch;
+
 
     public Animator animator;
     private Rigidbody2D rb;
     public Collider2D standingCollider;
     public Transform Ceiling;
+    public Transform Grounds;
     public LayerMask Layer;
-    
-    const float ceilingRadius = 0.2f;
+
+    const float Radius = 0.2f;
     public float moveSpeed = 1;
     public float jumpForce = 5;
     public float crouchSpeed = 1;
+    public int extraJumps = 1;
+    public float dashDistance = 15f;
+    int jumpCount = 0;
+    float jumpCoolDown;
+    bool isGround;
+    bool isCrouch;
+    bool isDashing;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //Jump
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+        checkGrounded();
+
+        //Dash Left
+        if (Input.GetButtonDown("Dash") && Input.GetKey(KeyCode.A)) 
+        {
+            StartCoroutine(Dash(-1f));
+        }
+        if (Input.GetButtonDown("Dash") && Input.GetKey(KeyCode.D))
+        {
+            StartCoroutine(Dash(1f));
+        }
     }
 
     // Update is called once per frame
@@ -35,50 +61,69 @@ public class PlayerController : MonoBehaviour
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        //Jump
-        if (Input.GetKey(KeyCode.W) && Ground)
-            Jump();
-
         //Crouch
-        if (Input.GetButton("Crouch") && Ground)
-            crouch = true;
+        if (Input.GetButton("Crouch") && isGround)
+            isCrouch = true;
         else
-            crouch = false;
-        if (!crouch)
+            isCrouch = false;
+        if (!isCrouch)
         {
-            if (Physics2D.OverlapCircle(Ceiling.position, ceilingRadius, Layer))
+            if (Physics2D.OverlapCircle(Ceiling.position, Radius, Layer))
             {
-                crouch = true;
+                isCrouch = true;
             }
         }
         Crouch();
-
         animator.SetBool("Walk", horizontalInput != 0);
-        animator.SetBool("Ground", Ground);
-        animator.SetBool("Crouch", crouch);
+        animator.SetBool("Ground", isGround);
+        animator.SetBool("Crouch", isCrouch);
+        animator.SetBool("Dash", isDashing);
 
-        if(crouch)
+        if (isCrouch)
             rb.velocity = new Vector2(horizontalInput * crouchSpeed, rb.velocity.y);
-        else
+        else if (!isDashing)
             rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
     }
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        animator.SetTrigger("Jump");
-        Ground = false;
+        if (isGround || jumpCount < extraJumps)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            animator.SetTrigger("Jump");
+            jumpCount++;
+        }
     }
 
     private void Crouch()
     {
-        standingCollider.enabled = !crouch;
+        standingCollider.enabled = !isCrouch;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    IEnumerator Dash (float direction)
     {
-        if (collision.gameObject.tag == "Ground")
-            Ground = true;
+        isDashing = true;
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
+        rb.AddForce(new Vector2(dashDistance * direction, 0), ForceMode2D.Impulse);
+        float gravity = rb.gravityScale;
+        rb.gravityScale = 0;
+        yield return new WaitForSeconds(0f);
+        isDashing = false;
+        rb.gravityScale = gravity;
+    }
+
+    private void checkGrounded()
+    {
+        if (Physics2D.OverlapCircle(Grounds.position, 0.5f, Layer))
+        {
+            isGround = true;
+            jumpCount = 0;
+            jumpCoolDown = Time.time + 0.2f;
+        }
+        else if (Time.time < jumpCoolDown)
+            isGround = true;
+        else
+            isGround = false;
     }
 
 }
