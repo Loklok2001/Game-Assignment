edf7ea5499aa37188db4494a1f9ba24acf6ee52f
Zerceus <86751682+Zerceus@users.noreply.github.com>
2021-08-24 15:46:04 +0800

    jump

--- GameAssignmentProject/Assets/Script/PlayerController.cs
+++ GameAssignmentProject/Assets/Script/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 1;
     public float jumpForce = 5;
+    private bool Ground;
     public Animator animator;
     private Rigidbody2D rb;
 
@@ -13,21 +14,39 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
-        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
-
         float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+
+        //Flip player
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
 
         rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
 
-        if (Input.GetKey(KeyCode.W))
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (Input.GetKey(KeyCode.W) && Ground)
+            Jump();
 
+        animator.SetBool("Walk", horizontalInput != 0);
+        animator.SetBool("Ground", Ground);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        animator.SetTrigger("Jump");
+        Ground = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            Ground = true;
     }
 }
