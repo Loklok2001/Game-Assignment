1c2f807bee76116c67a1d44288a5cc99cbad379d
Loklok2001 <huilok2001@gmail.com>
2021-08-26 11:51:42 +0800

    storeItemInInventory

--- GameAssignmentProject/Assets/Script/InventorySystem.cs
+++ GameAssignmentProject/Assets/Script/InventorySystem.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class InventorySystem : MonoBehaviour
 {
     public List<GameObject> items = new List<GameObject>();
+    bool isOpen = false;
 
     public GameObject ui_window;
     public Image[] items_images;
@@ -14,11 +15,41 @@ public class InventorySystem : MonoBehaviour
     public Image decription_Image;
     public Text decription_Text;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ToggleInventory();
+        }
+    }
+
+    void ToggleInventory()
+    {
+        isOpen = !isOpen;
+        ui_window.SetActive(isOpen);
+    }
+
     public void PickUp(GameObject item)
     {
         items.Add(item);
+        Update_Inventory();
     }
 
+    void Update_Inventory()
+    {
+        HideAll();
+        for(int i =0; i < items.Count; i++)
+        {
+            items_images[i].sprite = items[i].GetComponent<SpriteRenderer>().sprite;
+            items_images[i].gameObject.SetActive(true);
+        }
+    }
 
+    void HideAll()
+    {
+        foreach (var i in items_images) { 
+            i.gameObject.SetActive(false); 
+        }
+    }
 
 }
