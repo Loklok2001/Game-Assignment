8b611f6e69e3cf7667c692fe6cfd01814dd28b39
Loklok2001 <huilok2001@gmail.com>
2021-08-30 17:07:16 +0800

    Ui icon

--- GameAssignmentProject/Assets/Script/InventorySystem.cs
+++ GameAssignmentProject/Assets/Script/InventorySystem.cs
@@ -54,7 +54,7 @@ public class InventorySystem : MonoBehaviour
 
     }
 
-    void ToggleInventory()
+    public void ToggleInventory()
     {
         isOpen = !isOpen;
         ui_window.SetActive(isOpen);
--- GameAssignmentProject/Assets/Script/PauseMenu.cs
+++ GameAssignmentProject/Assets/Script/PauseMenu.cs
@@ -32,7 +32,7 @@ public class PauseMenu : MonoBehaviour
         GameIsPause = false;
     }
 
-    void Pause()
+    public void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
