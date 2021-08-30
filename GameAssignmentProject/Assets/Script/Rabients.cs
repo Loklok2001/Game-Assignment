077466a920b086dc850ae1961cdd0433dde61cf7
Zerceus <86751682+Zerceus@users.noreply.github.com>
2021-08-31 02:08:32 +0800

    A

--- GameAssignmentProject/Assets/Script/Rabients.cs
+++ GameAssignmentProject/Assets/Script/Rabients.cs
@@ -4,12 +4,123 @@ using UnityEngine;
 
 public class Rabients : MonoBehaviour
 {
+    public Transform rayCast;
+    public LayerMask raycastMask;
+    public float rayCastLength;
+    public float attackDistance;
+    public float moveSpeed;
+    public float timer;
 
-    // Start is called before the first frame update
-    void Start()
+    private RaycastHit2D hit;
+    private GameObject target;
+    private Animator animator;
+    private float distance;
+    private bool attackMode;
+    private bool inRange;
+    private bool cooling;
+    private float intTimer;
+
+    private void Awake()
     {
+        intTimer = timer;
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    private void Update()
+    {
+        if (inRange)
+        {
+            hit = Physics2D.Raycast(rayCast.position, Vector2.left, rayCastLength, raycastMask);
+            RayCastDebugger();
+        }
+
+        if (hit.collider != null)
+            EnemyLogic();
+        else if (hit.collider == null)
+            inRange = false;
+
+        if(inRange == false)
+        {
+            animator.SetBool("canWalk", false);
+            StopAttack();
+        }
+    }
+
+    void EnemyLogic()
+    {
+        distance = Vector2.Distance(transform.position, target.transform.position);
+        if (distance > attackDistance)
+        {
+            Move();
+            StopAttack();
+        }
+        else if (attackDistance >= distance && cooling == false)
+            Attack();
+
+        if (cooling)
+        {
+            Cooldown();
+            animator.SetBool("Attack", false);
+        }
+    }
+
+    void Move()
+    {
+        animator.SetBool("canWalk", true);
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("RabientAttackLeft"))
+        {
+            Vector2 targetPosition = new Vector2(target.transform.position.x, transform.position.y);
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    void Cooldown()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0 && cooling && attackMode)
+        {
+            cooling = false;
+            timer = intTimer;
+        }
+    }
+
+    void Attack()
+    {
+        timer = intTimer;
+        attackMode = true;
+
+        animator.SetBool("canWalk", false);
+        animator.SetBool("Attack", true);
+    }
+
+    void StopAttack()
+    {
+        cooling = false;
+        attackMode = false;
+        animator.SetBool("Attack", false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            target = collision.gameObject;
+            inRange = true;
+        }
+    }
+
+    void RayCastDebugger()
+    {
+        if (distance > attackDistance)
+            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.red);
+        else if(attackDistance > distance)
+            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.green);
+    }
+
+    public void TriggerCooling()
+    {
+        cooling = true;
+    }
 }
 
