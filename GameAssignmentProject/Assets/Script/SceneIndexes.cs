68d3032793b1cdb818aa12cd17b210b23bf8ee2b
Loklok2001 <huilok2001@gmail.com>
2021-09-04 20:51:20 +0800

    a

---
+++ GameAssignmentProject/Assets/Script/SceneIndexes.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SceneIndexes
+{
+    LOADING = 0,
+    MAINMENU = 1,
+    STAGE1 = 2,
+    STAGE2 = 3
+}
