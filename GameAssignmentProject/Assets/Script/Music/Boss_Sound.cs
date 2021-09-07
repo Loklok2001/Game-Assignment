1cfecfb53bd870c435b1714d595903cefc312f9c
Zerceus <86751682+Zerceus@users.noreply.github.com>
2021-09-07 14:39:16 +0800

    boss

---
+++ GameAssignmentProject/Assets/Script/Music/Boss_Sound.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Sound : MonoBehaviour
+{
+    public AudioClip bossHit;
+    public AudioClip bossDie;
+    static AudioSource audioSrc;
+
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    public void Boss_died_sound()
+    {
+        audioSrc.PlayOneShot(bossDie);
+    }
+
+    public void Boss_hited_sound()
+    {
+        audioSrc.PlayOneShot(bossHit);
+    }
+}
